428e49cfa660456ea66dcb55a043891179985391
Jáchym Šulman <jachym.sulman@gmail.com>
2026-05-15 13:45:26 +0200

    add empty PlayerController

---
+++ src/main/java/com/jeykym/pot/api/PlayerController.java
@@ -0,0 +1,23 @@
+package com.jeykym.pot.api;
+
+import com.jeykym.pot.dto.CreatePlayerRequest;
+import com.jeykym.pot.dto.PlayerDTO;
+import com.jeykym.pot.model.Player;
+import com.jeykym.pot.service.PlayerService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/players")
+public class PlayerController {
+
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+}
