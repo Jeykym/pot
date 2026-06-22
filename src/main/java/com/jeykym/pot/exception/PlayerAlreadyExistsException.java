5fc802684515868806862ac21978b192d4b21ab9
Jáchym Šulman <jachym.sulman@gmail.com>
2026-06-22 19:29:37 +0200

    make duplicate player return HTTP 400 status

--- src/main/java/com/jeykym/pot/exception/PlayerAlreadyExistsException.java
+++ src/main/java/com/jeykym/pot/exception/PlayerAlreadyExistsException.java
@@ -3,7 +3,7 @@ package com.jeykym.pot.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class PlayerAlreadyExistsException extends RuntimeException {
     public PlayerAlreadyExistsException(String name) {
         super("Player with name '" + name + "' already exists");
