56b876cf12d67ca1bebf28bac00294fdb94cdbdb
Jáchym Šulman <jachym.sulman@gmail.com>
2026-06-26 23:30:02 +0200

    make invalid field exception generic

--- src/main/java/com/jeykym/pot/exception/customException/InvalidFieldException.java
+++ src/main/java/com/jeykym/pot/exception/customException/InvalidFieldException.java
@@ -4,11 +4,12 @@ import com.jeykym.pot.exception.PotException;
 import org.springframework.http.HttpStatus;
 
 public class InvalidFieldException extends PotException {
-    public InvalidFieldException() {
-        super(
-                HttpStatus.BAD_REQUEST,
-                "PLAYER_NAME_IS_BLANK",
-                "Player's name cannot be blank"
-        );
+
+    public InvalidFieldException(String message) {
+        super(HttpStatus.BAD_REQUEST, "INVALID_FIELD", message);
+    }
+
+    public InvalidFieldException(String errorCode, String message) {
+        super(HttpStatus.BAD_REQUEST, errorCode, message);
     }
 }
